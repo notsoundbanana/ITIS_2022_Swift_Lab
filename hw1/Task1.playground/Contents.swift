edecd6024df375e3e61693475e0cad357c3d26dd
notSoundBanana <soundbanana1@gmail.com>
2022-07-02 01:45:41 +0300

    Added Homework 1

---
+++ hw1/Task1.playground/Contents.swift
@@ -0,0 +1,95 @@
+protocol HomeworkService {
+    // Функция деления с остатком, должна вернуть в первой части результат деления, во второй части остаток.
+    func divideWithRemainder(_ x: Int, by y: Int) -> (Int, Int)
+    
+    // Функция должна вернуть числа фибоначчи.
+    func fibonacci(n: Int) -> [Int]
+    
+    // Функция должна выполнить сортировку пузырьком.
+    func sort(rawArray: [Int]) -> [Int]
+    
+    // Функция должна преобразовать массив строк в массив первых символов строки.
+    func firstLetter(strings: [String]) -> [Character]
+    
+    // Функция должна отфильтровать массив по условию, которое приходит в параметре `condition`. (Нельзя юзать `filter` у `Array`)
+    func filter(array: [Int], condition: ((Int) -> Bool)) -> [Int]
+}
+
+
+struct Task1: HomeworkService {
+    
+    func divideWithRemainder(_ x: Int, by y: Int) -> (Int, Int) {
+        return (x / y, x % y)
+    }
+    
+    func fibonacci(n: Int) -> [Int] {
+        if n == 0 {
+            return [0]
+        }
+        else if n == 1 {
+            return [1]
+        }
+        var fibNumbers = [0, 1]
+        
+        for i in 2...n {
+            fibNumbers.append(fibNumbers[i - 1] + fibNumbers[i - 2])
+        }
+        return fibNumbers
+    }
+    
+    func sort(rawArray: [Int]) -> [Int] {
+        guard rawArray.count > 1 else {
+            return rawArray
+        }
+        
+        var result = rawArray
+        
+        for _ in 0..<result.count{
+            for j in 0..<result.count-1{
+                if result[j] > result[j + 1]{
+                    result.swapAt(j, j+1)
+                }
+            }
+        }
+        return result
+    }
+    
+    func firstLetter(strings: [String]) -> [Character] {
+        var result = [Character]()
+        
+        for i in 0..<strings.count {
+            if strings[i].count > 1 {
+                result.append(strings[i].first!)
+            }
+            else{
+                
+            }
+        }
+        return result
+    }
+    
+    
+    func filter(array: [Int], condition: ((Int) -> Bool)) -> [Int] {
+        var result = [Int]()
+        
+        for i in 0..<array.count{
+            if condition(array[i]){
+                result.append(array[i])
+            }
+        }
+        return result
+    }
+}
+
+let condition: (Int) -> Bool = { number in
+    return number >= 0
+}
+
+let task1 = Task1()
+
+print(task1.divideWithRemainder(10,by: 3))
+print(task1.fibonacci(n: 10))
+print(task1.sort(rawArray: [2, 3, 4, 1]))
+print(task1.firstLetter(strings: ["abc", "", "dfasdf"]))
+print(task1.filter(array: [-1, 1, 2, 3, 0, -2, -3], condition: condition))
+
---
+++ hw1/Task2.playground/Contents.swift
@@ -0,0 +1,148 @@
+class Person: Talk, Dance{
+    fileprivate var name: String
+    fileprivate var surname: String
+    fileprivate var subname: String
+    fileprivate var age: Int
+    
+    init(name: String, surname: String, subname: String, age: Int){
+        self.name = name
+        self.surname = surname
+        self.subname = subname
+        self.age = age
+    }
+    
+    func greeting() {
+        print("Здрасте")
+    }
+    
+    func dance() {
+        print("ТАНЦУЮ")
+    }
+}
+
+
+class Visitor: Person{
+    var books: [Book] = []
+    
+    override func greeting() {
+        print("Здравствуйте")
+    }
+    
+    override func dance() {
+        print("Я не могу танцевать, я посетитель бибиотеки")
+    }
+    
+    func addBook(book: Book){
+        books.append(book)
+    }
+}
+
+
+class Staff: Person{
+    private var tables: [Table] = []
+    private var chairs: [Chair] = []
+    
+    override func greeting() {
+        print("Приветствую Вас в Национальной Библиотеке")
+    }
+    
+    override func dance() {
+        print("Я не могу танцевать, я работник бибиотеки")
+    }
+    
+    func addTable(table: Table){
+        tables.append(table)
+    }
+    
+    func addChair(chair: Chair){
+        chairs.append(chair)
+    }
+    
+}
+
+class Administrator: Person{
+    public let post: Post = .administrator
+    public var cabinetNumber: Int
+    
+    init(name: String, surname: String, subname: String, age:Int, cabinetNumber: Int) {
+        self.cabinetNumber = cabinetNumber
+        super.init(name: name, surname: surname, subname: subname, age: age)
+    }
+    
+    override func greeting() {
+        print("Здравствуйте, я администратор. Чем могу быть полезным?")
+    }
+    
+    override func dance() {
+        print("Я могу танцевать, но только у себя в кабинете")
+    }
+}
+
+class PreAdministrator: Person{
+    public let post: Post = .preDirector
+    public var cabinetNumber: Int
+    
+    init(name: String, surname: String, subname: String, age:Int, cabinetNumber: Int) {
+        self.cabinetNumber = cabinetNumber
+        super.init(name: name, surname: surname, subname: subname, age: age)
+    }
+    
+    override func greeting() {
+        print("Здравствуйте, я заместитель администратор. Чем могу быть полезным?")
+    }
+    
+    override func dance() {
+        print("Я могу танцевать, но только у себя в кабинете")
+    }
+}
+
+
+enum Post{
+    case administrator
+    case preDirector
+}
+
+enum Weight{
+    case heavy
+    case light
+}
+
+protocol Talk{
+    func greeting()
+}
+
+protocol Dance{
+    func dance()
+}
+
+
+struct Table{
+    var numberOfLegs: Int
+    var weight: Weight = .heavy
+    
+    init(numberOfLegs: Int){
+        self.numberOfLegs = numberOfLegs
+    }
+    
+}
+
+struct Book{
+    var name: String
+    
+    init(name: String){
+        self.name = name
+    }
+}
+
+struct Chair{
+    var material: String
+    var weight: Weight = .light
+    
+    init(material: String){
+        self.material = material
+    }
+}
+
+var staff = Staff(name: "name", surname: "surname", subname: "subname", age: 90)
+print(staff.age)
+staff.greeting()
